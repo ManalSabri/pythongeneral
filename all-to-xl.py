d49668c658c84dd506d39dccf2cee6a7810e760f
ManalSabri <142030264+ManalSabri@users.noreply.github.com>
2023-08-17 14:19:45 +0100

    Create all-to-xl.py

---
+++ all-to-xl.py
@@ -0,0 +1,78 @@
+import mysql.connector
+from openpyxl import Workbook
+import argparse
+
+def add_sheet_to_workbook(wb, sheet_name, results, column_names):
+    # Create a new worksheet with the specified name
+    ws = wb.create_sheet(title=sheet_name)
+
+    # Add headers to the worksheet
+    for col_num, column_title in enumerate(column_names, 1):
+        col_letter = ws.cell(row=1, column=col_num)
+        col_letter.value = column_title
+
+    # Add data rows to the worksheet
+    for row_num, row_data in enumerate(results, 2):
+        for col_num, cell_data in enumerate(row_data, 1):
+            cell = ws.cell(row=row_num, column=col_num)
+            cell.value = cell_data
+
+def main(output_file):
+    # Database connection details
+    db_config = {
+        'host': 'host1',
+        'database': 'db1',
+        'user': 'user1',
+        'password': 'pass1'
+    }
+
+    # Connect to the database
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor()
+
+    # List of your multiline complex queries
+    queries = [
+        """
+        -- Your first query here
+        """,
+        """
+        -- Your second query here
+        """,
+        """
+        -- Your third query here
+        """,
+        """
+        -- Your fourth query here
+        """,
+        """
+        -- Your fifth query here
+        """
+    ]
+
+    # Create a new workbook
+    wb = Workbook()
+    # Remove the default sheet created by openpyxl
+    wb.remove(wb.active)
+
+    # Execute each query and add its results to a new sheet in the workbook
+    for index, query in enumerate(queries, 1):
+        cursor.execute(query)
+        results = cursor.fetchall()
+        column_names = [desc[0] for desc in cursor.description]
+        add_sheet_to_workbook(wb, f"Query_{index}", results, column_names)
+
+    # Save the workbook to the specified file
+    wb.save(output_file)
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+
+    print(f"Data saved to {output_file}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Save multiple database query results to an XLSX file.")
+    parser.add_argument("output_file", help="Name of the output XLSX file.")
+    args = parser.parse_args()
+    
+    main(args.output_file)
